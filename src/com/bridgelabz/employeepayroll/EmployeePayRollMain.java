fa396bdf802581c693436fc15df3e12ed7f75e59
om9022 <omkarchikane607@gmail.com>
2021-08-06 00:53:34 +0530

    Adeded: to watch perticular directory

--- src/com/bridgelabz/employeepayroll/EmployeePayRollMain.java
+++ src/com/bridgelabz/employeepayroll/EmployeePayRollMain.java
@@ -2,10 +2,8 @@ package com.bridgelabz.employeepayroll;
 
 public class EmployeePayRollMain {
 
-
 	public static void main(String[] args) 
 	{
-
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
 		System.out.println("Welcolme to employee payRoll");
 		employeePayrollService.readEmployeeDataFromConsole();
